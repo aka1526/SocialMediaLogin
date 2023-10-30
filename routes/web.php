995361492739233faa590a8d6a446850ec1719ad
akachai <akachai@PQSERVER.LOCAL>
2023-10-30 11:20:40 +0700

    Add new file

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/policy', function () {
+    return view('policy');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
