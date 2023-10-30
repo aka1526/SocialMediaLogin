e428095fcd764ec52b4f1b89ea55d6f46ab8dad1
akachai <akachai@PQSERVER.LOCAL>
2023-10-30 12:35:55 +0700

    Add new file

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Support\Facades\Auth;
-use App\User;
+use App\Models\User;
 
 class LoginController extends Controller{
     //All providers login
