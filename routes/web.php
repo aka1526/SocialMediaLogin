e036df435fc6882a0d97816313491659e744d826
akachai <akachai@PQSERVER.LOCAL>
2023-10-30 11:14:56 +0700

    Add new file

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
 use App\Http\Controllers\ProfileController;
-use Auth\LoginController;
+use App\Http\Controllers\Auth\LoginController;
 
 /*
 |--------------------------------------------------------------------------
